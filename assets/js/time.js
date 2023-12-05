6472b2930a11ff982f9717bde950fce08c83ab29
RumiAxolotl <rumiaxolotl@gmail.com>
2023-12-05 13:21:29 +0700

    Revert "add seconds to clock"
    This reverts commit 98a2f881cd5872c4e560058abb2b7d41e292b43b.

--- assets/js/time.js
+++ assets/js/time.js
@@ -5,33 +5,27 @@
 
 window.onload = displayClock();
 function displayClock() {
-  const months = [
-    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-  ];
+	const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-  const now = new Date();
-  const month = months[now.getMonth()];
-  const day = now.getDate();
-  const minutes = ("0" + now.getMinutes()).slice(-2);
-  let hours = now.getHours();
-  let ampm = "";
-  let seconds = ("0" + now.getSeconds()).slice(-2);
+	var d = new Date();
+	var mm = monthNames[d.getMonth()];
+	var dd = d.getDate();
+	var min = (mins = ('0' + d.getMinutes()).slice(-2));
+	var hh = d.getHours();
+	var ampm = '';
 
-  if (CONFIG.twelveHourFormat) {
-    ampm = hours >= 12 ? " pm" : " am";
-    hours = hours % 12 || 12;
-  }
+	if (CONFIG.twelveHourFormat) {
+		ampm = hh >= 12 ? ' pm' : ' am';
+		hh = hh % 12;
+		hh = hh ? hh : 12;
+	}
 
-  document.getElementById("hour").innerText = hours;
-  const separators = document.querySelectorAll("div#separator");
-  separators.forEach((separator) => {
-    separator.innerHTML = " : ";
-  });
-  document.getElementById("minutes").innerText = minutes + ampm;
-  document.getElementById("seconds").innerText = seconds;
-  document.getElementById("month").innerText = month;
-  document.getElementById("day").innerText = day;
+	document.getElementById('hour').innerText = hh;
+	document.getElementById('separator').innerHTML = ' : ';
+	document.getElementById('minutes').innerText = min + ampm;
 
-  setTimeout(displayClock, 1000);
+	document.getElementById('month').innerText = mm;
+	document.getElementById('day').innerText = dd;
+
+	setTimeout(displayClock, 1000);
 }
